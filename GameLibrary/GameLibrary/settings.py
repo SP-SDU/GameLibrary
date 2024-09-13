8fdb881ea2d3faa3e936c3655a1bbc2c7d760f69
Mathias <tisen2013@gmail.com>
2024-09-13 02:14:45 +0200

    Excluding db and secret key from repo
    excluded:
    .env
    db.sqlite3

--- GameLibrary/GameLibrary/settings.py
+++ GameLibrary/GameLibrary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import posixpath
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2b22cd79-be5d-4fa2-95fe-6cdc01d1054b'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
