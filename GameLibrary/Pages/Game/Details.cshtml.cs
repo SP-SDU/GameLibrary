ab95ed4a31ea1bc10b2a7f6ac6efa393151d3294
Ahmad Mohammad Al-Qadiri <ahalq21@student.sdu.dk>
2024-11-12 13:09:36 +0100

    Added Game and review model
    Added Game Model
    Added Review Model
    Created CRUD for game model

---
+++ GameLibrary/Pages/Game/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GameLibrary.Data;
+using GameLibrary.Models;
+
+namespace GameLibrary.Pages.Game
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly GameLibrary.Data.ApplicationDbContext _context;
+
+        public DetailsModel(GameLibrary.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Game Game { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var game = await _context.Games.FirstOrDefaultAsync(m => m.Id == id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Game = game;
+            }
+            return Page();
+        }
+    }
+}
