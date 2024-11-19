7ead9b018f270090813da9e3744b8d7502cdb91b
Mathias <tisen2013@gmail.com>
2024-11-19 08:20:19 +0100

    Js changes
    Move image preview to site.js, since image preview is likely to be used multiple places.
    
    Remove unused js:
    * details.js
    * library.js
    
    Change site.css to simple.css in order to switch to Tailwind and DaisyUI for UI
    Can be undone if needed

--- GameLibrary/wwwroot/js/site.js
+++ GameLibrary/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function previewImage(event) {
+    const files = event.target.files;
+    if (files.length > 0) {
+        const preview = document.getElementById('imagePreview');
+        preview.src = URL.createObjectURL(files[0]);
+        preview.style.display = 'block';
+    }
+}
