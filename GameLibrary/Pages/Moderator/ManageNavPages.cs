f9b10795e8f725c71e3052091964bc44a58958f8
Mathias <tisen2013@gmail.com>
2024-12-08 18:46:56 +0100

    Add a navigation pages file for moderator
    * Include navigation logic for Reviews and Users
    * Define methods to get navigation class for Index, Reviews, and Users pages
    * Implement logic to determine active page based on ViewContext

---
+++ GameLibrary/Pages/Moderator/ManageNavPages.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GameLibrary.Pages.Moderator;
+
+public static class ManageNavPages
+{
+    public static string Index => "Index";
+    public static string Reviews => "Reviews/Index";
+    public static string Users => "Users/Index";
+
+    public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+    public static string ReviewsNavClass(ViewContext viewContext) => PageNavClass(viewContext, Reviews);
+    public static string UsersNavClass(ViewContext viewContext) => PageNavClass(viewContext, Users);
+
+    public static string PageNavClass(ViewContext viewContext, string page)
+    {
+        var activePage = viewContext.ViewData["ActivePage"] as string
+            ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+        return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+    }
+}
